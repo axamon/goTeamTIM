b49c69fb5246e118b14f2251f3df01f5f31e01a5
alberto <alberto@localhost.localdomain>
2017-12-28 14:28:47 +0100

    OK, aggiungiamo la versione 1

---
+++ CDN/ElaboralogCDN.go
@@ -0,0 +1,359 @@
+package main
+
+import (
+	"net/url"
+	"projects/goTeamTIM/elasticTIM"
+	"runtime"
+	"strconv"
+	"sync"
+	"time"
+
+	"github.com/go-redis/redis"
+	"github.com/klauspost/pgzip"
+	//"compress/gzip"
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+
+	"strings"
+)
+
+const mapping = `
+{
+	"settings":{
+		"number_of_shards": 1,
+		"number_of_replicas": 0
+	},
+	"mappings":{
+		"log":{
+			"properties":{
+				"IdStr":{
+					"type":"keyword"
+				},
+				"ScreenName":{
+					"type":"keyword"
+				},
+				"FullText":{
+					"type":"text",
+					"store": true,
+					"fielddata": true
+				},
+				"image":{
+					"type":"keyword"
+				},
+				"CreatedAt":{
+					"type":"date"
+				},
+				"tags":{
+					"type":"keyword"
+				},
+				"location":{
+					"type":"keyword"
+				},
+				"suggest_field":{
+					"type":"completion"
+				}
+			}
+		}
+	}
+}`
+
+var (
+	Redispwd = os.Getenv("REDIS_PWD")
+
+	//Creazione client Redis
+	RedisClient = redis.NewClient(&redis.Options{ //connettiti a Redis server
+		Addr:     "localhost:6379",
+		Password: Redispwd, // no password set
+		DB:       0,        // use default DB
+	})
+
+	wg sync.WaitGroup
+
+	//Listalog è il nome della lista in redis dove accodare i record json
+	Listalog = "cdnrecords"
+)
+
+func init() {
+	//Verifica che Redis risponda
+	_, errore := RedisClient.Ping().Result()
+	if errore != nil {
+		fmt.Println("Qualcuno ha spento Redis? O sbagli passord? Essere umano che leggi...RIPARA!")
+		fmt.Println("la pwd per redis va impostata così: export REDIS_PWD=***")
+		os.Exit(1)
+	}
+}
+
+type Log struct {
+	Type         string `json:"type"`
+	Time         string `json:"time"`
+	TTS          int    `json:"tts"`
+	SEIp         string `json:"seip"`
+	Clientip     string `json:"clientip"`
+	Request      string `json:"req"`
+	Bytes        int    `json:"bytes"`
+	Method       string `json:"method"`
+	Urlschema    string `json:"urlschema"`
+	Urlhost      string `json:"urlhost"`
+	Urlpath      string `json:"urlpath"`
+	Urlquery     string `json:"urlquery"`
+	Urlfragment  string `json:"urlfragment"`
+	Mime         string `json:"mime"`
+	Ua           string `json:"ua"`
+	ServerIP     string `json:"serverip,omitempty"`
+	BytesRead    int    `json:"bytesread,omitempty"`
+	BytesToRead  int    `json:"bytestoread,omitempty"`
+	AssetSize    int    `json:"assetsize,omitempty"`
+	Status       string `json:"status,omitempty"`
+	IngestStatus string `json:"ingeststatus,omitempty"`
+}
+
+//Accesslog è il Template dei log Transaction
+type Accesslog struct {
+	//Hash        string
+	Type     string
+	Time     string
+	TTS      int
+	SEIp     string
+	Clientip string
+	Request  string
+	Bytes    int
+	Method   string
+	//Url         string
+	Urlschema   string
+	Urlhost     string
+	Urlpath     string
+	Urlquery    string
+	Urlfragment string
+	Mime        string
+	Ua          string
+}
+
+//Ingestlogtest è il Template dei log ingestion
+type Ingestlogtest struct {
+	Type string
+	//Hash         string
+	Time string
+	//URL          string
+	SEIp         string
+	Urlschema    string
+	Urlhost      string
+	Urlpath      string
+	Urlquery     string
+	Urlfragment  string
+	ServerIP     string
+	BytesRead    int
+	BytesToRead  int
+	AssetSize    int
+	Status       string
+	IngestStatus string
+}
+
+//Ingestlog è il template dei log ingestion
+type Ingestlog struct {
+	Type string
+	//Hash             string
+	Time string
+	//URL              string
+	SEIp             string
+	Urlschema        string
+	Urlhost          string
+	Urlpath          string
+	Urlquery         string
+	Urlfragment      string
+	FailOverSvrList  string
+	ServerIP         string
+	BytesRead        int
+	BytesToRead      int
+	AssetSize        int
+	DownloadComplete string
+	DownloadTime     string
+	ReadCallBack     string
+	Status           string
+	Mime             string
+	Revaldidation    string
+	CDSDomain        string
+	ConnectionInfo   string
+	IngestStatus     string
+	RedirectedURL    string
+	OSFailoverAction string
+	BillingCookie    string
+}
+
+// var wg = sizedwaitgroup.New(250) //massimo numero di go routine per volta
+
+func Leggizip(file string, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	//var wgr sync.WaitGroup
+	f, err := os.Open(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+	// gr, err := gzip.NewReader(f)
+	gr, err := pgzip.NewReaderN(f, 1024, 2048) //sfrutta il gzip con steroidi che legge nel futuro per andare più veloce e meglio assai del nomale gzip
+
+	if err != nil { //se però si impippa qualcosa allora blocca tutto
+		log.Fatal(err)
+		os.Exit(1)
+	}
+
+	fileelements := strings.Split(file, "_") //prende il nome del file di log e recupera i campi utili
+	Type := fileelements[1]                  //qui prede il tipo di log
+	SEIp := fileelements[3]                  //qui prende l'ip della cache
+	if Type == "accesslog" {                 //se il tipo di log è "accesslog"
+		scan := bufio.NewScanner(gr)
+		var saltariga int //per saltare le prime righe inutili
+		for scan.Scan() {
+			if saltariga < 2 { //salta le prime due righe
+				scan.Text()
+				saltariga = saltariga + 1
+				continue
+			}
+			line := scan.Text()
+			//wgr.Add(1)
+			//go func() {
+			//	defer wgr.Done()
+
+			s := strings.Split(line, "\t")
+			if len(s) < 5 { // se i parametri sono meno di 20 allora ricomincia il loop, serve a evitare le linee che non ci interessano
+				return
+			}
+			t, err := time.Parse("[02/Jan/2006:15:04:05.000-0700]", s[0]) //converte i timestamp come piacciono a me
+			if err != nil {
+				fmt.Println(err)
+			}
+			Time := t.Format("2006-01-02T15:04:05.000Z") //idem con patate questo è lo stracazzuto ISO8601 meglio c'è solo epoch
+			// fmt.Println(Time)
+			u, err := url.Parse(s[6]) //prendi una URL, trattala male, falla a pezzi per ore...
+			if err != nil {
+				log.Fatal(err)
+			}
+			TTS, _ := strconv.Atoi(s[1])
+			Clientip := s[2]
+			Request := s[3]
+			Bytes, _ := strconv.Atoi(s[4])
+			Method := s[5]
+			//Url := s[6]
+			Urlschema := u.Scheme
+			Urlhost := u.Host
+			Urlpath := u.Path
+			Urlquery := u.RawQuery
+			Urlfragment := u.Fragment
+			//gestione url finita
+			Mime := s[7]
+			Ua := s[8]
+
+			elerecord := Accesslog{
+				Type:        Type,
+				Time:        Time,
+				TTS:         TTS,
+				SEIp:        SEIp,
+				Clientip:    Clientip,
+				Request:     Request,
+				Bytes:       Bytes,
+				Method:      Method,
+				Urlschema:   Urlschema,
+				Urlhost:     Urlhost,
+				Urlpath:     Urlpath,
+				Urlquery:    Urlquery,
+				Urlfragment: Urlfragment,
+				Mime:        Mime,
+				Ua:          Ua}
+
+			elerecord2, _ := json.Marshal(elerecord)
+			elerecord3 := string(elerecord2)
+
+			errlpush := RedisClient.LPush("cdnrecords", elerecord3).Err()
+			if errlpush != nil {
+				fmt.Println("Non si è potuto inserire su redis")
+				os.Exit(200)
+			}
+		}
+	}
+
+	if Type == "ingestlog" {
+		scan := bufio.NewScanner(gr) //mettiamo tutto in un buffer che è rapido
+
+		var saltariga int //per saltare le prime righe inutili
+		for scan.Scan() {
+			if saltariga < 2 { //salta le prime due righe
+				scan.Text()
+				saltariga = saltariga + 1
+				continue
+			}
+			line := scan.Text()
+
+			s := strings.Split(line, " ") //splitta le linee secondo il delimitatore usato nel file di log, cambiare all'occorrenza
+
+			if len(s) < 20 { // se i parametri sono meno di 20 allora ricomincia il loop, serve a evitare le linee che non ci interessano
+				return
+			}
+
+			t, err := time.Parse("[02/Jan/2006:15:04:05.000-0700]", s[0]) //quant'è bello parsare i timestamp in go :)
+			if err != nil {
+				fmt.Println(err)
+			}
+			Time := t.Format("2006-01-02T15:04:05.000Z") //ISO8601 mon amour
+
+			//gestiamo le url secondo l'RFC ... non mi ricordo qual è
+			u, err := url.Parse(s[1]) //prendi una URL, trattala male, falla a pezzi per ore...
+			if err != nil {
+				log.Fatal(err)
+			}
+			//URL := s[1]
+			Urlschema := u.Scheme
+			Urlhost := u.Host
+			Urlpath := u.Path
+			Urlquery := u.RawQuery
+			Urlfragment := u.Fragment
+			ServerIP := s[3]
+			BytesRead, _ := strconv.Atoi(s[4])   //trasforma il valore in int
+			BytesToRead, _ := strconv.Atoi(s[5]) //trasforma il valore in int
+			AssetSize, _ := strconv.Atoi(s[6])   //trasforma il valore in int
+			Status := s[10]
+			IngestStatus := s[15]
+
+			elerecord := Log{
+				Time:         Time,
+				SEIp:         SEIp,
+				Urlschema:    Urlschema,
+				Urlhost:      Urlhost,
+				Urlpath:      Urlpath,
+				Urlquery:     Urlquery,
+				Urlfragment:  Urlfragment,
+				ServerIP:     ServerIP,
+				BytesRead:    BytesRead,
+				BytesToRead:  BytesToRead,
+				AssetSize:    AssetSize,
+				Status:       Status,
+				IngestStatus: IngestStatus}
+			fmt.Println(elerecord)
+
+		}
+		//fmt.Printf("%+v\n", l)
+
+	}
+	return //terminata la Go routine!!! :)
+}
+
+func main() {
+
+	//trace.Start(os.Stdout)
+	//defer trace.Stop()
+
+	runtime.GOMAXPROCS(runtime.NumCPU()) //esegue una go routine su tutti i processori
+
+	for _, file := range os.Args[1:] {
+		fmt.Println(file)
+		wg.Add(1)
+		go Leggizip(file, &wg)
+	}
+	wg.Wait()
+	elasticTIM.IngestaInElastic("http://127.0.0.1:9200", "cdn", "log", Listalog, mapping)
+
+	return
+}
---
+++ elasticTIM/IngestaInElastic.go
@@ -0,0 +1,141 @@
+package elasticTIM
+
+import (
+	"context"
+	"crypto/md5"
+	"encoding/hex"
+	"fmt"
+	"os"
+	"sync"
+
+	"github.com/go-redis/redis"
+	"github.com/olivere/elastic"
+)
+
+var (
+	redispwd = os.Getenv("REDIS_PWD")
+	//Creazione client Redis
+	clientR = redis.NewClient(&redis.Options{ //connettiti a Redis server
+		Addr:     "localhost:6379",
+		Password: redispwd, // no password set
+		DB:       0,        // use default DB
+	})
+)
+
+var wgg sync.WaitGroup
+
+//IngestaInElastic preleva dalla "lista" in redis gli elementi da ingestare in Elasticsearch secondo il "mapping" fornito
+//"elastichost" è l'host di elasticsearch da usare, "index" è l'index di destinazione (se non esiste viene creato) e il "tipo" deve essere in regola col mapping
+func IngestaInElastic(elastichost, index, tipo, lista, mapping string) {
+
+	//Verifica che Redis risponda
+	_, errore := clientR.Ping().Result()
+	if errore != nil {
+		fmt.Println("Qualcuno ha spento Redis? O sbagli passord? Essere umano che leggi...RIPARA!")
+		fmt.Println("la pwd per redis va impostata così: export REDIS_PWD=***")
+		os.Exit(1)
+	}
+
+	ctx := context.Background()
+
+	//Istanzia client per Elasticsearch
+	client, err := elastic.NewClient(elastic.SetURL(elastichost))
+	//.elastic.SetBasicAuth("user", "secret"))
+
+	_, _, errela := client.Ping(elastichost).Do(ctx)
+	if errela != nil {
+		// Handle error
+		panic(errela)
+	}
+	//fmt.Printf("Elasticsearch returned with code %d and version %s\n", code, info.Version.Number)
+	// Use the IndexExists service to check if a specified index exists.
+	exists, err := client.IndexExists(index).Do(ctx)
+	if err != nil {
+		// Handle error
+		panic(err)
+	}
+	if !exists {
+		// Create a new index.
+		createIndex, err := client.CreateIndex(index).BodyString(mapping).Do(ctx)
+		if err != nil {
+			// Handle error
+			panic(err)
+		}
+		if !createIndex.Acknowledged {
+			// Not acknowledged
+		}
+	}
+
+	//Creazione client Elasticsearch per inserimenti massivi
+	cb := elastic.NewBulkService(client)
+	if err != nil {
+		panic(err)
+	}
+	//Sembrerebbe dover ciclare all'infinito ma ci sono i break nel loop
+	for {
+		//numero di record nella lista di redis
+		numrecords, _ := clientR.LLen(lista).Result()
+		//trasformazione da int64 a int
+		num := int(numrecords)
+
+		//stampa a video il numero di item nella lista di redis
+		fmt.Println(num)
+
+		//facciamo dei mazzi
+		mazzo := 5000   //Quanti record da gestire insieme
+		numroutine := 5 //Quante Go routine usare
+
+		//Dividiamo il carico dell'ingestamento su più go routine
+		//per dare un minimo di informazioni su Kibana a ogni refresh
+		if num > mazzo {
+			for n := 0; n < numroutine; n++ {
+				wgg.Add(1) //aggiunge uno al syncgroup
+				go func() {
+					defer wgg.Done()
+					for i := 0; i < (mazzo / numroutine); i++ {
+
+						recordjson, _ := clientR.RPop(lista).Result()
+
+						//fmt.Println(recordjson) //scommentare per troubleshoting
+						hasher := md5.New()                         //prepara a fare un hash
+						hasher.Write([]byte(recordjson))            //hasha tutta la linea
+						Hash := hex.EncodeToString(hasher.Sum(nil)) //estrae l'hash md5sum in versione quasi human readable
+						//Hash fungerà da indice del record in Elasticsearch, quindi si evitato i doppi inserimenti
+						req := elastic.NewBulkIndexRequest().Index(index).Type(tipo).Id(Hash).Doc(recordjson)
+						cb.Add(req)
+					}
+					//per scrivere effetticamente il mazzo su elasticsearch
+					_, err = cb.Do(ctx)
+					if err != nil {
+						fmt.Println(err)
+					}
+					return //fine della go rountine
+				}()
+				fmt.Println("Avviata go routine: ", n) //scommentare per troubleshoting
+			}
+			wgg.Wait() //Attende che finiscano tutte le goroutine
+		}
+		//se gli elementi in lista sono meno di mazzo si gestiscono uno per volta
+		if num <= mazzo {
+			for i := 0; i < num; i++ {
+				recordjson, _ := clientR.RPop(lista).Result()
+
+				//fmt.Println(recordjson)
+				hasher := md5.New()                         //prepara a fare un hash
+				hasher.Write([]byte(recordjson))            //hasha tutta la linea
+				Hash := hex.EncodeToString(hasher.Sum(nil)) //estrae l'hash md5sum in versione quasi human readable
+				//Hash fungerà da indice del record in Elasticsearch, quindi si evitato i doppi inserimenti
+				req := elastic.NewBulkIndexRequest().Index(index).Type(tipo).Id(Hash).Doc(recordjson)
+				cb.Add(req)
+			}
+			_, err = cb.Do(ctx)
+			if err != nil {
+				fmt.Println(err)
+			}
+			//il break è importante se no si alluuuupaaaaa tutto
+			break
+		}
+		continue
+	}
+	return
+}
