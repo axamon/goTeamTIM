d44bcb8176a157686afcefdd023bbfdbe1d3469f
alberto <alberto@Host-001.homenet.telecomitalia.it>
2018-01-09 12:50:38 +0100

    revolution

--- CDN/elaboralog/creaindice.go
+++ CDN/elaboralog/creaindice.go
@@ -3,12 +3,45 @@ package elaboralog
 import (
 	"context"
 	"fmt"
-	"io/ioutil"
 	"os"
 	"time"
 
 	"github.com/olivere/elastic"
 )
+
+const mapping = `{
+		"settings":{
+			"number_of_shards": 10,
+			"number_of_replicas": 0
+		},
+		"mappings":{
+			"accesslog":{
+				"_source": {
+					"enabled": false
+				  },
+				"properties":{
+					"Time":{
+						"type":"date"
+					},
+					"Clientip":{
+						"type":"ip"
+					},
+					"SEIP":{
+						"type":"ip"
+					},
+					"HTTPStatus":{
+						"type":"keyword"
+					},
+					"Urlhost":{
+						"type":"keyword"
+					}
+				}
+			}
+		}
+	}`
+
+/* 	dat, _ := ioutil.ReadFile("mapping.json")
+   	Mapping := string(dat) */
 
 //var elastichost = "http://127.0.0.1:9200"
 
@@ -37,12 +70,12 @@ func creaindice(elastichost, index string) {
 	}
 	if !exists {
 		// Create a new index.
-		dat, _ := ioutil.ReadFile("mapping.json")
-		mapping := string(dat)
+		/* dat, _ := ioutil.ReadFile("mapping.json")
+		mapping := string(dat) */
 		//fmt.Println(mapping)
 		createIndex, err := client.CreateIndex(index).BodyString(mapping).Do(ctx)
 
-		time.Sleep(1 * time.Second)
+		time.Sleep(10 * time.Second)
 		if err != nil {
 			// Handle error
 			panic(err)
